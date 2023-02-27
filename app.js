7a5964a91be65c70af947aa89461bd542419cee1
Ify Okafor <ifeomasandra13@gmail.com>
2023-02-27 16:23:06 +0100

    Updated app

--- app.js
+++ app.js
@@ -13,31 +13,37 @@ function addNewTask() {
     var taskTime = document.getElementById("taskTime").value;
 
     let taskArr = [];
-    taskArr.push(taskName);
+    taskArr.push({
+        task: taskName,
+        time: taskTime,
+    });
 
     taskArr.forEach(function(item) {
         let li = document.createElement("li");    
         li.innerHTML = `<div>
                             <div class="d-flex justify-content-end">
-                                <i class="bi bi-pencil-square me-3 font-25" id="taskEdit" onclick="editTask()"></i>
-                                <i class="bi bi-x-square font-25" id="taskDelete" onclick="deleteTask()"></i>                                
+                                <i class="bi bi-pencil me-3 font-25" id="taskEdit" onclick="editTask(this)"></i>
+                                <i class="bi bi-x-lg font-25" id="taskDelete" onclick="deleteTask(this)"></i>                                
                             </div>
                             <div class="d-flex w-100 justify-content-between">
                                 <input type="checkbox" id="cbox">
-                                <label for="cbox" class="ms-2" style="width: 90%;">${taskName} </br><span class="text-secondary">${taskTime}</span></label>                                
+                                <label for="cbox" class="ms-2 editBox" style="width: 90%;">${taskName} </br><span class="text-secondary">${taskTime}</span></label>                                
                             </div>
+                            
                         </div>`;
         li.classList.add("task-item");        
         taskList.appendChild(li);    
-    })       
+    })          
 };
 taskBtn.addEventListener("click", addNewTask);
 
+let deleteTask = (e) => {
+    e.parentElement.parentElement.parentElement.remove();    
+};
 
-function deleteTasks() {
-    let tasks = document.getElementById("tasks");
-
-    tasks.removeChild(taskList);
-    return addNewTask;
+function deleteTasks() {    
+    taskList.innerHTML = "";
+    return taskArr;    
 }
 taskDeleteBtn.addEventListener("click", deleteTasks);
+
